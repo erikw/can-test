d5fe5bce7883dbe2d4d8e94f402e28ad400e4ad5
Robot <robot@blackbox03.(none)>
2012-02-04 15:09:19 +0100

    It now drives in a circle

--- src/can_test.cpp
+++ src/can_test.cpp
@@ -4,25 +4,82 @@
 #include <cob_canopen_motor/CanDriveHarmonica.h>
 #include <cob_base_drive_chain/CanCtrlPltfCOb3.h>
 
+// void rotate_rel(CanCtrlPltfCOb3 &ctrlCob, double deg, double speed)
+// {
+// 	bool positive = (speed >= 0);
+// 	double angleRadStart = 0;
+// 	double velRadS = 0;
+//  	ctrlCob.requestDriveStatus // TODO request some how
+	
+
+// 	ctrlCob.getGearPosVelRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, &angleRadStart, &velRadS);
+// 	std::cout << "start angle = " << angleRadStart << std::endl;
+
+// 	double curAngle = angleRadStart;
+// 	while (positive && (curAngle < (deg + angleRadStart)) || !positive && (curAngle > (deg + angleRadStart))) {
+// 		std::cout << "angle = " << curAngle << std::endl;
+// 		ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, speed);
+// 		ctrlCob.getGe
+// 		usleep(10000);
+// 	}
+// 	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, 0);
+// }
+
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "can_test");
 	ros::NodeHandle n;
-	//sleep(10);
-	//can_ctrl ctrlCob("IniFiles/");
 	CanCtrlPltfCOb3 ctrlCob("IniFiles/");
 	
 	ctrlCob.initPltf();
 	std::cout << "Done with initPltf()" << std::endl;	
-		
-	//ctrlCob.setVelGearRadS(CANNODE_WHEEL1STEERMOTOR, 0.5);
+
+	std::cout << "Now running own commands;" << std::endl;	
+
+
+	//ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1DRIVEMOTOR, -2);
+	//ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, -0.5);
+	// for (int i = 0; i < 20; ++i) {
+	// 	usleep(500000);
+	// 	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1DRIVEMOTOR, -2);
+	// }
+	// for (int i = 0; i < 20; ++i) {
+	// 	usleep(500000);
+	// 	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, -2);
+	// }
 
 	//ctrlCob.requestDriveStatus();
-	//std::cout << "Now sleeping..." << std::endl;
-	//sleep(10);
 
+	// Spin 360 deg.
+	/* ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, 3.141592); */
+	/* usleep(2000000); */
+	/* ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, 0); */
+
+	// std::cout << "rotating +0.25" << std::cout; 
+	// rotate_rel(ctrlCob, .25, 0.5);
+
+	//std::cout << "rotating -0.25" << std::cout; 
+	//rotate_rel(.25, 0.5);
+	
+	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, -0.7);
+	usleep(800000);
+	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, 0);
+	
+	for (int i = 0; i < 30; ++i) {
+	  ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1DRIVEMOTOR, 2);
+	  usleep(800000);
+	}
+	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1DRIVEMOTOR, 0);
+
+	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, 0.7);
+	usleep(800000);
+	ctrlCob.setVelGearRadS(CanCtrlPltfCOb3::CANNODE_WHEEL1STEERMOTOR, 0);
+
+
+
+
+	std::cout<< "Plattform is shuting down." << std::endl;
 	ctrlCob.shutdownPltf();	
-	std::cout<< "Plattform shut down." << std::endl;
 
 
 	return 0;
